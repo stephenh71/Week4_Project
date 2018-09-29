e401fc7ed026be531638331bbda275f74694b5e7
Stephen Hart <stephen@cardswitcher.co.uk>
2018-09-29 09:23:19 +0100

    Transactions MVP working

---
+++ models/tag.rb
@@ -0,0 +1,53 @@
+require('pg')
+require_relative('../db/sql_runner')
+
+class Tag
+
+  attr_accessor :title, :budget
+  attr_reader :id
+
+  def initialize( options )
+    @id = options['id'].to_i
+    @title = options['title']
+    @budget = options['budget'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO tags (title) VALUES ($1) RETURNING id"
+    values = [@title]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]["id"].to_i
+  end
+
+  def update()
+    sql = "UPDATE tags SET title = $1 WHERE id = $2"
+    values = [@title, @id]
+    results = SqlRunner.run(sql, values)
+    return Tag.new(results[0])
+  end
+
+  def delete()
+    sql = "DELETE FROM tags WHERE id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM tags"
+    results = SqlRunner.run(sql)
+    return results.map {|result|Tag.new(result)}
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM tags"
+    SqlRunner.run(sql)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM tags WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    return Tag.new(results[0])
+  end
+
+end
